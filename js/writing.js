4126c06532a2e2c153b69a8f4c8de39a0466e523
dooli1971039 <dooli9819@gmail.com>
2022-11-14 06:22:01 +0900

    ✨ feat : Implement category selection function

---
+++ js/writing.js
@@ -0,0 +1,42 @@
+import {Data} from "./data.js";
+
+let category=document.querySelector("#category");
+Data.forEach((item)=>{
+    /*카테고리 */
+    let li=document.createElement("li");
+    li.innerText=item.category;
+    li.classList.add("category");
+    li.addEventListener("click",clickCategory);
+    category.appendChild(li);
+
+    /* 카테고리 안의 detail*/
+    let ul=document.createElement("ul");
+    item.details.forEach((detail)=>{
+        let insideLi=document.createElement("li");
+        insideLi.innerText=detail;
+        insideLi.classList.add("details")
+        insideLi.classList.add("hidden")
+        insideLi.addEventListener("click",clickDetail);
+        ul.appendChild(insideLi)
+    });
+    category.appendChild(ul);
+})
+
+function clickCategory(event){
+    /*css 수정 */
+    console.log(category.childNodes)
+    category.childNodes.forEach((item)=>item.classList.remove("choosen"));
+    event.target.classList.add("choosen");
+
+    /* js 하위 요소 추가 */
+    category.childNodes.forEach((item)=>{
+        if(!item.classList.contains("category")){
+            item.childNodes.forEach((i)=>i.classList.add("hidden"))
+        }
+    })
+    event.target.nextElementSibling.childNodes.forEach((item)=>item.classList.remove("hidden"))
+}
+
+function clickDetail(event){
+    console.log(event.target);
+}
